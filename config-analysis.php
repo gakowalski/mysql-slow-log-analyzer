1705609d52821706ca723286c25b2e7aaee822fb
Grzegorz Kowalski <grzegorz.adam.kowalski@outlook.com>
2023-11-07 13:55:23 +0100

    changed defaults and descriptions in config file

--- config-analysis.php
+++ config-analysis.php
@@ -5,14 +5,14 @@ $admin_password = '';
 $admin_host = 'localhost';
 
 $mysqldumpslow_cmd = 'mysqldumpslow';
-$long_query_time = 5; // seconds
+$long_query_time = 0.5; // seconds, can be float
 
-$innodb_monitor_minimum_calculation_time = 20; // seconds
+$innodb_monitor_minimum_calculation_time = 20; // seconds, used in tuning concurrency
 
 function get_analysis_parameter($name) {
     $parameters = [
-        'acceptable_number_of_rows' => 400, // number of rows
-        'fake_sleep_query' => false,       // boolean
+        'acceptable_number_of_rows' => 1000, // number of rows
+        'fake_sleep_query' => false,         // boolean, true works when acceptable rows = 0
     ];
     return $parameters[$name];
 };
